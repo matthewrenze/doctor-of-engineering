97294992765a2d9e7d5af321142dee70550c054b
Matthew Renze <matthew@matthewrenze.com>
2025-10-03 09:33:14 -0700

    Added tool card format experiment

---
+++ 3-tool-use/experiments/tool-card-formats/code/environments/env_factory.py
@@ -0,0 +1,33 @@
+from pandas import DataFrame
+from common.parameters import Parameters
+from environments.action_parser import ActionParser
+from environments.open_qa_env import OpenQAEnv
+from environments.mcqa_env import MCQAEnv
+from environments.textworld_env import TextWorldEnv
+from graders.grader_factory import GraderFactory
+from tools.tool_router import ToolRouter
+
+class EnvFactory:
+
+    def __init__(self):
+        self.grader_factory = GraderFactory()
+
+    def create(self, params: Parameters, eval: DataFrame):
+
+        # Create components
+        grader = self.grader_factory.create(params)
+        parser = ActionParser()
+        router = ToolRouter()
+
+        # Create environment
+        if params.env_name == "mcqa":
+            return MCQAEnv(params, eval, parser, router, grader)
+
+        elif params.env_name == "open-qa":
+            return OpenQAEnv(params, eval, parser, router, grader)
+
+        elif params.env_name == "textworld":
+            return TextWorldEnv(params, eval)
+
+        else:
+            raise ValueError(f"Unknown eval name: {params.env_name}")
---
+++ 3-tool-use/experiments/tool-card-formats/code/environments/textworld_env.py
@@ -0,0 +1,59 @@
+import re
+import textworld.gym
+
+class TextWorldEnv:
+    def __init__(self, params, evals):
+        self.params = params
+        self.evals = evals
+        self.env = None
+        self.episode = None
+        self.episode_id = 0
+        self.task = ""
+        self.step_index = 0
+        self.max_reward = 0.0
+
+    def reset(self, episode_id):
+        self.episode_id = episode_id
+        self.episode = self.evals.iloc[episode_id].to_dict()
+        game_file_path = self.episode["file_path"]
+
+        # Set the env info (what info the player has access to)
+        env_infos = textworld.EnvInfos(
+            objective=True,
+            admissible_commands=True,
+            description=True,
+            inventory=True,
+            feedback=True,
+            max_score=True,)
+
+        # Register the game
+        env_id = textworld.gym.register_game(
+            gamefile=game_file_path,
+            request_infos=env_infos,
+            max_episode_steps=self.params.max_steps)
+
+        # Create the environment
+        self.env = textworld.gym.make(env_id)
+
+        # Reset the environment
+        state, infos = self.env.reset()
+
+        # Remove the banner
+        state = "\n".join(state.split("\n")[23:])
+
+        self.task = infos.get("objective", "").strip()
+        self.step_index = 0
+        self.max_reward = infos["max_score"]
+
+        return self.task, state
+
+    def render(self):
+        self.env.render()
+
+    def step(self, action: str) -> (str, float, bool):
+        state, reward, is_done, infos = self.env.step(action)
+        state = re.sub(r'\n+', '\n', state.strip())
+        state = state.rstrip('>')
+        self.step_index += 1
+        reward = reward / self.max_reward
+        return state, reward, is_done
